eeedf70a099a042c571ea4fad91569c60e18c318
BT_3W2_22 <nathanz@nycstudents.net>
2022-10-28 09:26:14 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = myScanner.nextInt();
+        if (num <= 0) {
+            System.out.println("That is not a positive number!");
+        } else {
+            if ((num % 2) == 1) {
+                if (((num % 5) == 0) && ((num % 7) == 0)) {
+                    System.out.println(num + " is divisible by 5");
+                    System.out.println(num + " is divisible by 7");
+                } else if ((num % 5) == 0) {
+                    System.out.println(num + " is divisible by 5");
+                } else if ((num % 7) == 0) {
+                    System.out.println(num + " is divisible by 7");
+                } else {
+                    System.out.println(num + " is not divisible by both 5 and 7");
+                }
+            } else {
+                if (num > 1000) {
+                    System.out.println("Big even number!");
+                } else if (num > 100) {
+                    System.out.println("Medium even number!");
+                } else {
+                    System.out.println("Small even number!");
+                }
+            }
+        }
+    }
 }
--- src/ReturnError.java
+++ src/ReturnError.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ReturnError {
+public class ReturnError {
+    private int score;
+
+    public ReturnError(int score) {
+        this.score = score;
+    }
+
+    public String grade() {
+        if (score >= 90) {
+            return "A";
+        }
+        if (score >= 80) {
+            return "B";
+        }
+        if (score >= 70) {
+            return "C";
+        }
+        if (score >= 60) {
+            return "D";
+        }
+            return "F";
+    }
 }
+
